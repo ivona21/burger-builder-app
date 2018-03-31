d8723923962da4e26d749a194f69ace3f8d760ab
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-03-31 21:51:10 +0200

    burger is visible finally

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = () => {
-
+const burger = (props) => {
+    return (
+        <div className={styles.Burger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="salad"/>
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="cheese"/>
+            <BurgerIngredient type="bacon"/>
+            <BurgerIngredient type="salad"/>
+            <BurgerIngredient type="bacon"/>
+            <BurgerIngredient type="cheese"/>
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
 }
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { PureComponent } from "react";
 import Aux from "../../hoc/Auxiliary";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends PureComponent {
     render () {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger />
                 <div>Build Controls</div>
             </Aux>
         );
