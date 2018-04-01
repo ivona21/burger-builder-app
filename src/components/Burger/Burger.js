f94a196f3e8c8735a27d12f5a5c426eb256d5ab0
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-04-01 12:15:05 +0200

    array flattened to check condition

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,7 +2,9 @@ import React from "react";
 import styles from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = (props) => {
+const burger = (props) => {  
+    let message = null;    
+   
     const transformedIngredients = Object.keys(props.ingredients).map(
         (ingredientName) => {
             return [...Array(props.ingredients[ingredientName])].map(
@@ -13,12 +15,20 @@ const burger = (props) => {
                 }
             )
         }
-    )
+    ).reduce((accumulator, currentValue) => {
+        return accumulator.concat(currentValue);
+    });
+
+    if (transformedIngredients.length == 0){
+        message = <p>Please add some ingredients!</p>
+    }
+    console.log(transformedIngredients);
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top" />
             {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
+            {message}
         </div>
     );
 }
