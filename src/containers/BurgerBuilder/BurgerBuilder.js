0cace507748682735b4763b48942e52daebb36fe
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-04-04 10:02:26 +0200

    adding and removing ingredients to burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,17 +36,42 @@ class BurgerBuilder extends PureComponent {
             ingredients: updatedIngredients,
             totalPrice: newPrice
         });
-    }
+    }    
 
-    removeIngredientHandler = (type) => {
-
+    removeIngredientHandler = (type) => {       
+        const oldCount = this.state.ingredients[type];
+        if (oldCount === 0){
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: newPrice
+        });
     }
 
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        }
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BulidControls />
+                <BulidControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                />
             </Aux>
         );
     }
